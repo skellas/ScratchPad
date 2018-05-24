a1c9037bd31313e6cb80da6524b30171e1e54775
Seth Kellas <sjkellas@gmail.com>
2018-05-24 14:18:17 -0400

    New Binary Tree traversals class

--- src/main/java/com/skellas/dsa/tree/BTTraversal.java
+++ src/main/java/com/skellas/dsa/tree/BTTraversal.java
@@ -5,10 +5,14 @@ import java.util.Deque;
 
 public class BTTraversal {
 
+    /**
+     * Iterate over each sibling before moving down into the
+     * right/left sub trees. This is accomplished using a queue.
+     * @param root
+     */
     public static void breadthFirst(BinaryTreeNode root) {
-        if (root == null) {
-            return;
-        }
+        testNodeIsNotNull(root);
+        // store all nodes in a queue
         Deque<BinaryTreeNode> queue = new ArrayDeque();
         // add() is the same as addLast()
         queue.add(root);
@@ -16,8 +20,7 @@ public class BTTraversal {
             // remember to remove and not just "get"
             // remove() is the same as removeFirst()
             BinaryTreeNode node = queue.remove();
-            System.out.println(node.toString());
-
+            print(node);
             if (node.hasLeft())
                 queue.add(node.getLeft());
             if (node.hasRight())
@@ -26,10 +29,14 @@ public class BTTraversal {
     }
 
 
-    public static void depthFirst(BinaryTreeNode root) {
-        if (root == null) {
-            return;
-        }
+    /**
+     * Pre Ordered Depth First Traversal, using a simple loop.
+     * This is accomplished via a stack.
+     * @param root
+     */
+    public static void depthFirstWithLoop(BinaryTreeNode root) {
+        testNodeIsNotNull(root);
+
         Deque<BinaryTreeNode> stack = new ArrayDeque();
         // push() is the same as addFirst()
         stack.push(root);
@@ -37,14 +44,76 @@ public class BTTraversal {
             // remember to remove and not just "get"
             // pop() is the same as removeFirst()
             BinaryTreeNode node = stack.pop();
-            System.out.println(node.toString());
-
-            if (node.hasLeft())
-                stack.push(node.getLeft());
+            print(node);
+            // this will end up adding right, then left
+            // which means that we will traverse left first, always
             if (node.hasRight())
                 stack.push(node.getRight());
+            if (node.hasLeft())
+                stack.push(node.getLeft());
         }
     }
 
+    /**
+     * Process node, then left, then right.
+     * @param root
+     */
+    public static void depthFirstPreOrderWithRecursion(BinaryTreeNode root) {
+        testNodeIsNotNull(root);
+        // start with the root before traversing
+        print(root);
+        // then the left node
+        if (root.hasLeft())
+            depthFirstPreOrderWithRecursion(root.getLeft());
+        // then the right node
+        if (root.hasRight())
+            depthFirstPreOrderWithRecursion(root.getRight());
+    }
+
+    /**
+     * Process left, then node, then right
+     * @param root
+     */
+    public static void depthFirstInOrderWithRecursion(BinaryTreeNode root) {
+        testNodeIsNotNull(root);
+        // start with the left node
+        if (root.hasLeft())
+            depthFirstInOrderWithRecursion(root.getLeft());
+        // then the root node
+        print(root);
+        // then the right node
+        if (root.hasRight())
+            depthFirstInOrderWithRecursion(root.getRight());
+    }
+
+    public static void depthFirstPostOrderWithRecursion(BinaryTreeNode root) {
+        testNodeIsNotNull(root);
+        // start with the left node
+        if (root.hasLeft())
+            depthFirstPostOrderWithRecursion(root.getLeft());
+        // then the right node
+        if (root.hasRight())
+            depthFirstPostOrderWithRecursion(root.getRight());
+        // then the root node
+        print(root);
+    }
+
+    /**
+     * SUPER PRETTY FORMATTING
+     * @param node
+     */
+    private static void print(BinaryTreeNode node) {
+        System.out.println(node.toString());
+    }
+
+    /**
+     * Welcome to the sledge hammer approach.
+     * Don't do something like this in a real project...
+     * @param node
+     */
+    private static void testNodeIsNotNull(BinaryTreeNode node) {
+        if (null == node)
+            throw new RuntimeException("Node Cannot Be Null");
+    }
 
 }
--- src/test/java/com/skellas/dsa/tree/BTTraversalTest.java
+++ src/test/java/com/skellas/dsa/tree/BTTraversalTest.java
@@ -7,21 +7,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class BTTraversalTest {
 
     private BinaryTreeNode createSimpleTree() {
-        BinaryTreeNode nodeACD = new BinaryTreeNode("D");
-        BinaryTreeNode nodeACE = new BinaryTreeNode("E");
-        BinaryTreeNode nodeAB = new BinaryTreeNode("B");
+        BinaryTreeNode nodeAEH = new BinaryTreeNode("H");
+        BinaryTreeNode nodeADG = new BinaryTreeNode("G");
+        BinaryTreeNode nodeADF = new BinaryTreeNode("F");
+        BinaryTreeNode nodeACE = new BinaryTreeNode("E", null, nodeAEH);
+        BinaryTreeNode nodeACD = new BinaryTreeNode("D", nodeADF, nodeADG);
         BinaryTreeNode nodeAC = new BinaryTreeNode("C", nodeACD, nodeACE);
+        BinaryTreeNode nodeAB = new BinaryTreeNode("B");
         return new BinaryTreeNode("A", nodeAB, nodeAC);
     }
 
     @Test
-    public void test_simple_tree_breadth_first_traversal() {
+    public void test_breadth_first_traversal() {
         BTTraversal.breadthFirst(createSimpleTree());
     }
 
     @Test
-    public void test_simple_tree_depth_first_traversal() {
-        BTTraversal.depthFirst(createSimpleTree());
+    public void test_depth_first_traversal_with_loop() {
+        BTTraversal.depthFirstWithLoop(createSimpleTree());
+    }
+
+    @Test
+    public void test_pre_order_depth_first_traversal() {
+        BTTraversal.depthFirstPreOrderWithRecursion(createSimpleTree());
+    }
+
+    @Test
+    public void test_in_order_depth_first_traversal() {
+        BTTraversal.depthFirstInOrderWithRecursion(createSimpleTree());
+    }
+
+    @Test
+    public void test_post_order_depth_first_traversal() {
+        BTTraversal.depthFirstPostOrderWithRecursion(createSimpleTree());
     }
 
 }
